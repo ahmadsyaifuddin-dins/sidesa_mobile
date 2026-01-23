68d20009b93c5f7841a1089325dee1e56f7cce95
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2026-01-23 18:39:23 +0800

    oke gak jadi pake Bloc pake getx aja

---
+++ lib/core/config/api_config.dart
@@ -0,0 +1,7 @@
+class ApiConfig {
+  // IP Laptop kamu: 192.168.43.208
+  static const String baseUrl = "http://192.168.43.208:8000/api";
+  
+  static const String login = "$baseUrl/login";
+  static const String user = "$baseUrl/user";
+}
---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+import 'app_routes.dart';
+// Nanti kita import view login di sini
+// import '../features/auth/views/login_view.dart';
+
+class AppPages {
+  static const INITIAL = Routes.LOGIN;
+
+  static final routes = [
+    // Nanti kita uncomment kalau view-nya sudah dibuat
+    // GetPage(
+    //   name: Routes.LOGIN,
+    //   page: () => const LoginView(),
+    // ),
+  ];
+}
